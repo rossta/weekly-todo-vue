8d0de17b353a074ee70bef79fb4b5f164b698e35
Ross Kaffenberger <rosskaff@gmail.com>
2017-10-31 08:54:59 -0400

    Add new fonts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -75,9 +75,12 @@ export default {
 </script>
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+  * {
+    /* font-family: 'Raleway', sans-serif; */
+  }
   h1, h2 {
-    font-weight: normal;
+    font-family: 'Signika', sans-serif;
   }
 
   h1 {
