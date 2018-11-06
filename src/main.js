eeadd91aeb07a7d6244802a79680482fe121e208
Ross Kaffenberger <rosskaff@gmail.com>
2018-11-06 09:40:47 -0500

    Upgrade to Vue CLI 3 without semantic-ui

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import 'semantic-ui-less/semantic.less';
-
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
@@ -11,9 +9,7 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  template: '<App/>',
-  components: { App },
-});
+  render: h => h(App),
+}).$mount('#app')
